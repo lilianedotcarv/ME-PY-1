a8dc4c7f60bdeb8a7e1481e11cffc1c5e19e6590
lilianedotcarv <menezesliliane16@gmail.com>
2026-09-14 16:15:35 -0300

    Add files via upload

---
+++ desafioextra.py
@@ -0,0 +1,37 @@
+try:
+    arquivo_vendas = open("vendas.txt", "a", encoding="utf-8")
+    log_erros = open("log_erros.txt", "a", encoding="utf-8")
+
+    produto = input("Nome do produto?")
+    quantidade = int(input("Quantidade so produto?"))
+    valor = float(input("Preço?"))
+
+    if not produto:
+        raise ValueError("O produto não pode ficar vazio.")
+
+    if quantidade <= 0:
+        raise ValueError("A quantidade deve ser maior que 0.")
+
+    if valor <= 0:
+        raise ValueError("O preço deve ser maior que 0.")
+
+
+    lucro = quantidade * valor
+
+    arquivo_vendas.write(
+        f"Produto: {produto} | "
+        f"Quantidade: {quantidade} | "
+        f"Preço: R$ {valor:.2f} | "
+        f"Lucro: R$ {lucro:.2f}\n"
+    )
+
+    print("Venda registrada com sucesso!")
+
+    arquivo_vendas.close()
+
+except ValueError as erro:
+    print("Erro:", erro)
+
+    with open("log_erros.txt", "a", encoding="utf-8") as log:
+        log.write("Erro")
+
