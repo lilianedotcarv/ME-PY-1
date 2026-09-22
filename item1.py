834b738a346cb3eaaf2739b45251d8cfe8844873
lilianedotcarv <menezesliliane16@gmail.com>
2026-09-21 21:40:06 -0300

    Update item1.py

--- item1.py
+++ item1.py
@@ -1,25 +1,24 @@
-veiculos = {}
-
-print("=====Cadastro de veículo=====")
+from datetime import datetime
 
 while True:
-    modelo_veiculo = input("Qual o modelo do veículo? ")
-        
-    if modelo_veiculo.lower() == 'sair':
-        break
+
+    registro = ()
+    print("=====Cadastro de veículo=====")
 
     try:
-        ano_veiculo = int(input("Qual o ano do veículo? (ex: 2026): "))
-        if ano_veiculo < 1884:
-            raise Exception("Escreva um numero maior que 1884!")
-            
-        km_rodados = float(input("Quantos quilometros rodados?: "))
-        if km_rodados < 0:
+        modelo = input("Qual o modelo do veículo? ")
+
+        ano_atual = datetime.now().year
+        ano = int(input("Qual o ano do veículo? (ex: 2026): "))
+        if ano < 1884 or ano > ano_atual:
+            raise Exception("Escreva um ano válido")
+           
+        km = float(input("Quantos quilometros rodados?: "))
+        if km < 0:
             raise Exception("Escreva uma quilometragem válida!")
          
-        veiculos[modelo_veiculo] = [ano_veiculo, km_rodados]
-
-        print(f"\nO veículo foi registrado!\nVeículo: {modelo_veiculo} | Ano: {ano_veiculo} | KM: {km_rodados}\n")
+        regitro = modelo, ano, km
+        print(f"\nO veículo foi registrado!\nVeículo: {modelo} | Ano: {ano} | KM: {km}\n")
 
     except ValueError:
         print("Digite apenas números!\n")
@@ -27,6 +26,5 @@ while True:
     except Exception as erro:
         print(f"Opa! Algo deu errado!\n{erro}\n")
 
-print("\n--- Lista de Veículos Cadastrados ---")
-for veiculo, valor in veiculos.items():
-    print(f"Veículo: {veiculo} | Ano: {valor[0]} | KM: {valor[1]}")
+    if modelo == 'sair':
+        break
